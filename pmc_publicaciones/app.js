fcf7b64a8ef29df04e63b9cb6c9739472014f4ba
Juan <jj.silva@uniandes.edu.co>
2018-10-18 06:11:25 -0500

    Publicaciones

---
+++ pmc_publicaciones/app.js
@@ -0,0 +1,46 @@
+// Basic Config
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+// Routes
+const publicacionRouter = require('./routes/publicacion');
+
+// App
+const app = express();
+
+// DB Cloud Atlas
+/*mongoose.connect('mongodb+srv://ArquiAdmin:ArquiAdmin@cluster0-alitq.mongodb.net/test?retryWrites=true',
+    {
+        useNewUrlParser: true
+    });
+*/
+
+//DB MongoDB
+mongoose.connect('mongodb://localhost:27017/Nidoo',
+    {
+      useNewUrlParser: true
+    });
+mongoose.connection.on('connected', function(err, resp){
+    console.log("Conectado a DB");
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/publicaciones', publicacionRouter);
+
+module.exports = app;
+
+//PORT
+const port = process.env.PORT || 8082;
+app.listen(port, '0.0.0.0', console.log(`Listening on port ${port}`));
